aeb641ebce2a9907b8675960cc4d188b9d551551
zainab7681051 <zainab7681051@gmail.com>
2022-12-12 23:36:58 +0300

    m

---
+++ index.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv')
+	.config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({
+	extended: false
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+require('./routes')(app)
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
